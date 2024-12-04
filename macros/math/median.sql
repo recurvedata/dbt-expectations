1d5e3fdccea3b0d35e305a6979136a77f0373076
JingFeng.Ren <jingfeng.ren@recurvedata.com>
2024-12-04 11:20:28 +0800

    Support [Median] in MySQL database
    *NOTE:
        Optional Arg [group by] does not take effect at present

--- macros/math/median.sql
+++ macros/math/median.sql
@@ -1,3 +1,28 @@
-{% macro median(field) %}
+{% macro median(model, field, expr_var) %}
+{{ adapter.dispatch('median', 'dbt_expectations') (model, field, expr_var) }}
+{% endmacro %}
+
+{% macro default__median(model, field, expr_var) %}
 {{ dbt_expectations.percentile_cont(field, 0.5) }}
+{% endmacro %}
+
+{% macro mysql__median(model, field, expr_var) %}
+(
+select
+	avg({{field}})
+from
+	(
+	select
+		{{field}},
+		@{{expr_var}} := @{{expr_var}} + 1 row_num
+	from
+		{{model}},
+		(
+		select
+			@{{expr_var}} := 0) t2
+	order by
+		{{field}}) t
+where
+	row_num between @{{expr_var}} / 2 and @{{expr_var}} / 2 + 1
+)
 {% endmacro %}
--- macros/schema_tests/aggregate_functions/expect_column_median_to_be_between.sql
+++ macros/schema_tests/aggregate_functions/expect_column_median_to_be_between.sql
@@ -6,8 +6,20 @@
                                                     strictly=False
                                                     ) %}
 
+
+{{ adapter.dispatch('median_expr_between', 'dbt_expectations') (model,
+                                                                column_name,
+                                                                min_value,
+                                                                max_value,
+                                                                group_by,
+                                                                row_condition,
+                                                                strictly
+                                                                ) }}
+{% endtest %}
+
+{% macro default__median_expr_between(model, column_name, min_value, max_value, group_by, row_condition, strictly) %}
 {% set expression %}
-{{ dbt_expectations.median(column_name) }}
+{{ dbt_expectations.median(model, column_name, 'a') }}
 {% endset %}
 {{ dbt_expectations.expression_between(model,
                                         expression=expression,
@@ -17,4 +29,33 @@
                                         row_condition=row_condition,
                                         strictly=strictly
                                         ) }}
-{% endtest %}
+{% endmacro %}
+
+{% macro mysql__median_expr_between(model, column_name, min_value, max_value, group_by, row_condition, strictly) %}
+{% set expression_a %}
+{{ dbt_expectations.median(model, column_name, 'a') }}
+{% endset %}
+{% set expression_b %}
+{{ dbt_expectations.median(model, column_name, 'b') }}
+{% endset %}
+{%- if min_value is none and max_value is none -%}
+{{ exceptions.raise_compiler_error(
+    "You have to provide either a min_value, max_value or both."
+) }}
+{%- endif -%}
+
+{%- set strict_operator = "" if strictly else "=" -%}
+
+{% set expression_min_max %}
+( 1=1
+{%- if min_value is not none %} and {{ expression_a | trim }} >{{ strict_operator }} {{ min_value }}{% endif %}
+{%- if max_value is not none %} and {{ expression_b | trim }} <{{ strict_operator }} {{ max_value }}{% endif %}
+)
+{% endset %}
+
+{{ dbt_expectations.expression_is_true(model,
+                                        expression=expression_min_max,
+                                        group_by_columns=group_by,
+                                        row_condition=row_condition)
+                                        }}
+{% endmacro %}
